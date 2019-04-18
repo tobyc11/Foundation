f7af0cac1c1c551b0b2dae36357dba6cf67c66cc
Toby Chen <chenyanjun912@hotmail.com>
2019-04-18 15:26:15 -0700

    Make FSpinLock std::mutex compatible

--- Public/SpinLock.h
+++ Public/SpinLock.h
@@ -15,6 +15,16 @@ public:
     }
 
     void Unlock() { LockFlag.clear(std::memory_order_release); }
+    
+    bool TryLock()
+    {
+        return !LockFlag.test_and_set(std::memory_order_acquire);
+    }
+
+    // To meet BasicLockable C++ named requirement
+    void lock() { Lock(); }
+    void unlock() { Unlock(); }
+    bool try_lock() { TryLock(); }
 
 private:
     alignas(64) std::atomic_flag LockFlag = ATOMIC_FLAG_INIT;
